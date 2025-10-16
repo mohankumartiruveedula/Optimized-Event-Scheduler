630ee0a02eea5d42d6cd10f641ef596570a1ad6d
Mohan Kumar Tiruveedula <mohankumartiruveedula01@gmail.com>
2025-10-16 22:30:50 +0530

    made some changes

--- Optimized-Event-Scheduler/main.cpp
+++ Optimized-Event-Scheduler/main.cpp
@@ -1,7 +1,5 @@
 #include <bits/stdc++.h>
 
-using json = nlohmann::json;
-
 int ub(int index,std::vector<std::vector<int>>& events){
     int req=events.size();
     int myend = events[index][1];
@@ -64,51 +62,6 @@ std::vector<int> maxValue(std::vector<std::vector<int>> events, int k) {
 }
 
 int main(){
-    httplib::Server svr;
-
-    svr.Post("/process",[](const httplib::Request &req,httplib::Response &res){
-        
-        std::vector<std::vector<int>> events;
-        int k = -1;
-
-        try{
-            json data = json::parse(req.body);
-            
-
-            for (const auto& item : data) {
-                if(k==-1){
-                    k = item.at("k").get<int>();
-                    continue;
-                }
-                auto description = item.at("description").get<std::string>();
-                auto startDate = item.at("startDate").get<int>();
-                auto endDate = item.at("endDate").get<int>();
-                auto quantity = item.at("quantity").get<int>();
-                events.push_back({startDate,endDate,quantity});
-            }
-            
-            
-
-        }catch(json::exception& e){
-            std::cerr << "JSON Parse Error: " << e.what() << std::endl;
-            res.status = 400; // Bad Request
-            res.set_content("Invalid JSON format", "text/plain");
-            return;
-        }
-
-        json response_json;
-
-        std::vector<int> result = maxValue(events,k);
-
-        response_json["totalValue"] = result[0];
-        
-        res.set_content(response_json.dump(), "application/json");
-        res.set_header("Access-Control-Allow-Origin", "*");
-    });
-
-    std::cout << "C++ Backend Server (with Array support) started on http://localhost:1234" << std::endl;
-    svr.listen("localhost", 8080);
 
     return 0;
-
 }
